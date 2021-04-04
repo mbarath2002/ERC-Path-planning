75277e773c501bbc60b9ea94bf7ba8c24df47f26
mbarath2002 <mbarath2002@gmail.com>
2021-04-05 01:10:41 +0530

    Update RRT.py

--- RRT.py
+++ RRT.py
@@ -6,29 +6,35 @@ import random
 def collision(node,obstacle_list):
 flag=0
 
+class Node(object):
+    def __init__(self, node):
+        self.parent = None
+
 def RRT(start,goal,obstacle_list):
 min=0
 max=10                           #bounds of region to generate random node
 rate=0.5                         #distance of node expansion when adding new node
 n=400
 list=[start]                     #list of nodes
-x=random.uniform(min,max)        #random node generation
-y=random.uniform(min,max)    
-rnd_node=(x,y)         
-dist= [(node[0]-rnd_node[0])**2 + (node[1]-rnd_node[1])**2 for node in list] #distance of nearest node in list
-min_pos=dist.index(min(dist)) #position noded to add to list
-nearest_node=list[min_pos]
-angle=math.atan2(y-nearest_node[1], x-nearest_node[0]) 
-new_node_x=nearest_node[0]+rate*math.cos(angle) #expanding nodes in that direction
-new_node_y=nearest_node[1]+rate*math.sin(angle)
-new_node=(new_node_x,new_node_y)
-list.append[new_node] #add new node to list
-d_x= new_node[0]-goal[0]
-d_y= new_node[1]-goal[1]
-d= math.sqrt(d_x**2 + d_y**2)
-if(d<=rate):
+while True:
+  x=random.uniform(min,max)      #random node generation
+  y=random.uniform(min,max)    
+  rnd_node=(x,y)         
+  dist= [(node[0]-rnd_node[0])**2 + (node[1]-rnd_node[1])**2 for node in list] #distance of nearest node in list
+  min_pos=dist.index(min(dist)) #position noded to add to list
+  nearest_node=list[min_pos]
+  angle=math.atan2(y-nearest_node[1], x-nearest_node[0]) 
+  new_node_x=nearest_node[0]+rate*math.cos(angle) #expanding nodes in that direction
+  new_node_y=nearest_node[1]+rate*math.sin(angle)
+  new_node=(new_node_x,new_node_y)
+  new_node=Node(new_node)
+  new_node.parent=min_pos
+  list.append[new_node] #add new node to list
+  d_x= new_node[0]-goal[0]
+  d_y= new_node[1]-goal[1]
+  d= math.sqrt(d_x**2 + d_y**2)
+  if(d<=rate):
   print("goal reached")
-else:
-  
+  break;
 
 def visualize(path,obstacle_list):
