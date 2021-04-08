f5731324a2af9188628a4c09234acc598abfa277
mbarath2002 <mbarath2002@gmail.com>
2021-04-09 00:21:05 +0530

    Update and rename RRT.py to rrt.py

--- rrt.py
+++ rrt.py
@@ -70,19 +70,4 @@ def visualize(path,obstacle_list):
         p.plot((path[i][0],path[i-1][0]),(path[i][1],path[i-1][1]),color='g')
         i=i-1
 
-def main():
-    print("Start")
-    start = (1, 1)
-    goal = (10, 10)
-    obstacle_list=[
-            [(2, 10), (7, 10), (6, 7), (4, 7), (4, 9), (2, 9)],
-            [(3, 1), (3, 6), (4, 6), (4, 1)],
-            [(7, 3), (7, 8), (9, 8), (9, 3)],
-        ]
-    path = RRT(start,goal,obstacle_list)
-    visualize(path,obstacle_list)
-    
-if __name__ == '__main__':
-    main()
-
 p.show()
